59c08b8b8c3717823c3929c62d7f11ca659a89a9
pmoukda <moukda_p@hotmail.com>
2025-12-01 15:02:59 -0500

    Enlever les codes commentées

--- frontend/src/pages/CreerCellier.jsx
+++ frontend/src/pages/CreerCellier.jsx
@@ -7,32 +7,27 @@ export default function CreerCellier() {
   const [nomCellier, setNomCellier] = useState("");
   const [erreurs, setErreurs] = useState({});
   const [message, setMessage] = useState("");
-
-
-
+  
+  
   const gererSoumission = async (e) => {
     e.preventDefault();
-
+    
     setErreurs({});
     setMessage("");
-
+    
     // Vérifier côté front que le champ n'est pas vide
     if (!nomCellier.trim()) {
       setErreurs({ nom: "Le nom du cellier est requis." });
       return;
     }
-
+    
     try {
-      /*const reponse = await axios.post("http://localhost:8000/api/celliers", {
-        nom: nomCellier,
-        user_id: 1,  // Remplacez par l'ID de l'utilisateur connecté
-      });*/
       const reponse = await api.post("/celliers", { nom: nomCellier });
-
+      
       setNomCellier("");
       setMessage(reponse.data.message || "Cellier créé avec succès !");    
-
-
+      
+      
     } catch (error) {
       if (error.response && error.response.data.errors) {
         setErreurs(error.response.data.errors);
@@ -41,45 +36,45 @@ export default function CreerCellier() {
       }
     }
   };
-
+  
   return (
     <section className="mt-20">
-      <form
-        className="flex flex-col space-y-4 p-4 bg-form rounded-lg"
-        onSubmit={gererSoumission}
-      >
-        <h1 className="text-4xl font-bold">Créer un cellier</h1>
-
-        <div className="flex flex-col mt-2 border-t border-gray-200 pt-5">
-          <label className="text-brown" htmlFor="nomCellier">
-            Nom du cellier
-          </label>
-          <input
-            className="px-2 py-1 bg-white rounded w-full focus:outline-none focus:border-red-300 focus:ring-1 focus:ring-red-300"
-            type="text"
-            id="nomCellier"
-            name="nom"
-            placeholder="Ex. : Cellier du sous-sol"
-            value={nomCellier}
-            onChange={(e) => setNomCellier(e.target.value)}
-          />
-          {erreurs.nom && <p className="text-red-500 pt-2">{erreurs.nom[0]}</p>}
-        </div>
-
-        {message && (
-          <p className="text-sm font-semibold text-green-700 pt-2">{message}</p>
-        )}
-
-        <input
-          className="bouton-accent"
-          type="submit"
-          value="Créer le cellier"
-        />
-
-        <Link className="underline mt-4 text-sm" to="/celliers">
-          Retour à mes celliers
-        </Link>
-      </form>
+    <form
+    className="flex flex-col space-y-4 p-4 bg-form rounded-lg"
+    onSubmit={gererSoumission}
+    >
+    <h1 className="text-4xl font-bold">Créer un cellier</h1>
+    
+    <div className="flex flex-col mt-2 border-t border-gray-200 pt-5">
+    <label className="text-brown" htmlFor="nomCellier">
+    Nom du cellier
+    </label>
+    <input
+    className="px-2 py-1 bg-white rounded w-full focus:outline-none focus:border-red-300 focus:ring-1 focus:ring-red-300"
+    type="text"
+    id="nomCellier"
+    name="nom"
+    placeholder="Ex. : Cellier du sous-sol"
+    value={nomCellier}
+    onChange={(e) => setNomCellier(e.target.value)}
+    />
+    {erreurs.nom && <p className="text-red-500 pt-2">{erreurs.nom[0]}</p>}
+    </div>
+    
+    {message && (
+      <p className="text-sm font-semibold text-green-700 pt-2">{message}</p>
+    )}
+    
+    <input
+    className="bouton-accent"
+    type="submit"
+    value="Créer le cellier"
+    />
+    
+    <Link className="underline mt-4 text-sm" to="/celliers">
+    Retour à mes celliers
+    </Link>
+    </form>
     </section>
   );
 }
