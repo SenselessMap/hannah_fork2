0dc0b14a51fbb4f46d3e75289b9c9c4acfe07ecd
pmoukda <moukda_p@hotmail.com>
2025-12-03 10:21:34 -0500

    Ajout du css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,41 +12,31 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
 
-
 function App() {
   return (
-    <>
-      <Header/>
-
-      <main className="pb-24">   {/* important pour ne pas cacher le contenu */}
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="pb-24 flex-1 px-4">
+        {" "}
+        {/* important pour ne pas cacher le contenu */}
         <Routes>
           <Route path="/inscription" element={<Inscription />} />
-        <Route path="/connexion" element={<Auth />} />
-        <Route path="/compte" element={<CompteUsager />} />
-        <Route path="/" element={<Catalogue />} />         
+          <Route path="/connexion" element={<Auth />} />
+          <Route path="/compte" element={<CompteUsager />} />
+          <Route path="/" element={<Catalogue />} />
 
-        <Route path="/produit/:id" element={<FicheProduit />} />      
-        <Route path="/user/:id/celliers" element={<CellierUtilisateur />} />
-        <Route path="/cellier/creer" element={<CreerCellier />} />
-        
+          <Route path="/produit/:id" element={<FicheProduit />} />
+          <Route path="/user/:id/celliers" element={<CellierUtilisateur />} />
+          <Route path="/cellier/creer" element={<CreerCellier />} />
 
-
-        <Route path="/produit/:id" element={<FicheProduit />} />
-        <Route path="/celliers" element={<CellierUtilisateur />} />
+          <Route path="/produit/:id" element={<FicheProduit />} />
+          <Route path="/celliers" element={<CellierUtilisateur />} />
         </Routes>
-      
-       
       </main>
-     <Footer />
-      <MenuMobile />   {/* ajout Menu application mobile */}
-      
-    </>
+      <Footer />
+      <MenuMobile /> {/* ajout Menu application mobile */}
+    </div>
   );
 }
 
 export default App;
-
-
-
-
-
